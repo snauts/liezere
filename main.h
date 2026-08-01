f6d55951fecab5e8902bf4ca9846fdab12afeab8
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2026-08-01 21:19:24 +0300

    change c64 memory mapping

--- main.h
+++ main.h
@@ -32,8 +32,8 @@ typedef unsigned short word;
 #endif
 
 #if defined(C64)
-#define SYMBOL(x) 	PTR(0x6600 + ((x) << 3))
-#define STAGING_AREA	PTR(0x6800)
+#define SYMBOL(x) 	PTR(0x6900 + ((x) << 3))
+#define STAGING_AREA	PTR(0x6a00)
 #endif
 
 #define POS(X, Y)	{ .x = X, .y = Y }
